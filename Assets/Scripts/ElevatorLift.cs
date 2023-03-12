f02c874e125eaa4fe67151e814eb60128026f0ae
Zerithax <Zerithax@gmail.com>
2023-03-12 17:46:43 -0500

    Lift Update
    - Added a Shack building for players to spawn in above the Cave. Shack contains the Elevator Lift and a new animated Steam Engine object that helps indicate the vertical position of the Lift.
    - Added a LocalPlayerTracker script that attaches a trigger to the player in order to track players' collision with the Lift.
    
    TODO:
    Due to the speed the Lift has to travel at, players frequently have to catch up with the lift when going down, and will phase through the lift partway through when going up. To combat this, Stations should be used but it appears that setting them to Immobilize is the only way, which may require a rebuild of the Character Controller. More research is needed.

---
+++ Assets/Scripts/ElevatorLift.cs
@@ -0,0 +1,149 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class ElevatorLift : UdonSharpBehaviour
+{
+    [Header("Lift Settings")]
+    [SerializeField] private GameObject liftObject;
+    [SerializeField] private Collider liftTrigger;
+    [SerializeField] private GameObject safetyNet;
+    [SerializeField] private float liftMinHeight;
+    [SerializeField] private float liftMaxHeight;
+    [SerializeField] private float liftCycleTime = 10f;
+    [SerializeField] private float playerResponseDelay = 3f;
+    [UdonSynced] private bool elevatorLerping;
+    [UdonSynced] private bool reachedSafetyNet;
+
+    [Header("Station")]
+    [SerializeField] private bool useStation;
+    [SerializeField] private VRCStation liftStation;
+
+    private float liftStartHeight;
+    private float elevatorCallTime;
+    private float elevatorPlayerCollisionTime;
+
+    [Header("Steam Engine Animations")]
+    [SerializeField] private GameObject spoolRope;
+    [SerializeField] private float ropeStartingX;
+    [SerializeField] private float ropeMaximumX;
+    [SerializeField] private int rotateAmount;
+    [SerializeField] private GameObject spool;
+    [SerializeField] private GameObject pulley;
+
+    private void Start() { if (liftStation == null) useStation = false; liftStartHeight = liftMinHeight; }
+
+    public override void OnPlayerJoined(VRCPlayerApi player) { InitElevator(); }
+
+    public void InitElevator()
+    {
+        if (liftObject.transform.localPosition.y == liftMaxHeight) return;
+
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
+
+        liftStartHeight = liftObject.transform.localPosition.y;
+        elevatorLerping = true;
+        reachedSafetyNet = false;
+        safetyNet.SetActive(true);
+        liftTrigger.enabled = false;
+
+        elevatorCallTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (elevatorLerping)
+        {
+            //Smoothly animate scalar from 0 - 1
+            float animationScalar = Mathf.SmoothStep(0, 1, (Time.time - (!reachedSafetyNet ? elevatorCallTime : elevatorPlayerCollisionTime)) / liftCycleTime);
+
+            //Elevator is on its way UP to the net
+            if (!reachedSafetyNet)
+            {
+                liftObject.transform.localPosition = new Vector3(0, (Mathf.Abs(liftStartHeight) + Mathf.Abs(liftMaxHeight)) * animationScalar + liftStartHeight, 0);
+
+                spoolRope.transform.localPosition = new Vector3((Mathf.Abs(ropeStartingX) + Mathf.Abs(ropeMaximumX)) * (1 - animationScalar) + ropeMaximumX, spoolRope.transform.localPosition.y, spoolRope.transform.localPosition.z);
+
+                spool.transform.localRotation = Quaternion.Euler(new Vector3(360 * -rotateAmount * animationScalar, 0, 90));
+                pulley.transform.localRotation = Quaternion.Euler(new Vector3(360 * -rotateAmount * animationScalar, 0, 90));
+
+                //When lift reaches maxHeight, prep to lerp down
+                if (liftObject.transform.localPosition.y == liftMaxHeight)
+                {
+                    //Wait for trigger enter before lowering
+                    elevatorLerping = false;
+
+                    //Next time elevatorLerping == true, lerp down instead
+                    reachedSafetyNet = true;
+                    safetyNet.SetActive(false);
+
+                    liftTrigger.enabled = true;
+                }
+            }
+            //Elevator is on its way DOWN to the cave
+            else
+            {
+                //Account for the delay
+                if (Time.time > elevatorPlayerCollisionTime)
+                {
+                    liftObject.transform.localPosition = new Vector3(0, (Mathf.Abs(liftStartHeight) + Mathf.Abs(liftMaxHeight)) * (1 - animationScalar) + liftStartHeight, 0);
+
+                    spoolRope.transform.localPosition = new Vector3((Mathf.Abs(ropeStartingX) + Mathf.Abs(ropeMaximumX)) * animationScalar + ropeMaximumX, spoolRope.transform.localPosition.y, spoolRope.transform.localPosition.z);
+
+                    spool.transform.localRotation = Quaternion.Euler(new Vector3(360 * rotateAmount * animationScalar, 0, 90));
+                    pulley.transform.localRotation = Quaternion.Euler(new Vector3(360 * rotateAmount * animationScalar, 0, 90));
+
+                    //When lift reaches minHeight, stop lerping
+                    if (liftObject.transform.localPosition.y == liftMinHeight)
+                    {
+                        elevatorLerping = false;
+                        liftTrigger.enabled = true;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        LocalPlayerTracker playerTracker = other.GetComponent<LocalPlayerTracker>();
+        if (playerTracker != null)
+        {
+            Debug.Log($"[{name}] Entered!");
+
+            //If the elevator isn't actively running, activate it and save the collision time
+            if (!elevatorLerping)
+            {
+                elevatorLerping = true;
+                elevatorPlayerCollisionTime = Time.time + playerResponseDelay;
+            }
+
+            if (useStation)
+            {
+                Vector3 enterPos = playerTracker.localPlayer.GetPosition();
+                Quaternion enterRot = playerTracker.localPlayer.GetRotation();
+
+                liftStation.UseStation(playerTracker.localPlayer);
+                playerTracker.localPlayer.TeleportTo(enterPos, enterRot);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        LocalPlayerTracker playerTracker = other.GetComponent<LocalPlayerTracker>();
+        if (playerTracker != null)
+        {
+            if (useStation)
+            {
+                Debug.Log($"[{name}] Exited!");
+
+                Vector3 enterPos = playerTracker.localPlayer.GetPosition();
+                Quaternion enterRot = playerTracker.localPlayer.GetRotation();
+
+                liftStation.ExitStation(playerTracker.localPlayer);
+                playerTracker.localPlayer.TeleportTo(enterPos, enterRot);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LocalPlayerTracker.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class LocalPlayerTracker : UdonSharpBehaviour
+{
+    //This class will manage tracking the player's interactions that will eventually be saved with NUSS
+    //This class will manage tracking and managing collisions for the player (since OnPlayerTriggerEnter doesn't work in client sim)
+
+    [SerializeField] public VRCPlayerApi localPlayer;
+    [SerializeField] private float heightOffset = 1;
+
+    private void Start() { localPlayer = Networking.LocalPlayer; }
+
+    private void Update()
+    {
+        Vector3 playerPos = localPlayer.GetPosition();
+        transform.position = new Vector3(playerPos.x, playerPos.y + heightOffset, playerPos.z);
+
+        transform.rotation = localPlayer.GetRotation();
+    }
+}
